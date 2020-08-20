8eaca4b9bc60dd284615669ea79a8ecc96506f76
Zoujinda <826835384@qq.com>
2020-08-20 21:42:10 +0800

    使用数组维护数据，view层动态渲染

--- web-ui/docs/.vuepress/components/live/liveList.vue
+++ web-ui/docs/.vuepress/components/live/liveList.vue
@@ -7,83 +7,33 @@
       :inside-name="'CONNECT'"
       :outside-name="i18n.interaction.LIVE.LIVE"
     ></common-banner>
-    <div class="liveList">
-      <div class="now-start">
-        <div class="now-start-tile">{{i18n.interaction.LIVE.WILLPLAYER}}</div>
-        <div class="now-start-content">
-          <div class="content-box">
-            <div class="left-content">
-              <img src="/img/live/zhllesson.png" alt />
-              <img src="/img/live/teacher-zhl.png" alt />
-              <img src="/img/live/player730.png" alt />
+    <div class="live_ist">
+      <div class="now_start">
+        <div class="now_start_title">{{i18n.interaction.LIVE.WILLPLAYER}}</div>
+        <div class="now_start_content">
+          <div class="content_box" v-for="(item,key) in nowArr" :key="key">
+            <div class="left_content">
+              <p class="live_title">{{item.LIVETITLE}}</p>
+              <p class="live_teacher">{{item.LIVETEACHER}}</p>
+              <p class="live_time">{{item.LIVETIME}}</p>
             </div>
-            <div class="right-content">
-              <img src="/img/live/zhl.png" alt />
-            </div>
-          </div>
-          <div class="content-box">
-            <div class="left-content">
-              <img src="/img/live/czdlesson.png" alt />
-              <img src="/img/live/teacher-czd.png" alt />
-              <img src="/img/live/player730.png" alt />
-            </div>
-            <div class="right-content">
-              <img src="/img/live/czd.png" alt />
+            <div class="right_content">
+              <img :src="item.PHOTOPATH" alt />
             </div>
           </div>
         </div>
       </div>
       <div class="replayer">
-        <div class="replayer-tile">{{i18n.interaction.LIVE.REPLAYER}}</div>
-        <div class="replayer-content">
-          <div class="content-box">
-            <div class="left-content">
-              <img src="/img/live/zyplesson.png" alt />
-              <img src="/img/live/teacher-zyp.png" alt />
-              <img src="/img/live/replayer728.png" alt />
+        <div class="replayer_tile">{{i18n.interaction.LIVE.REPLAYER}}</div>
+        <div class="replayer_content">
+          <div class="content_box" v-for="(item,key) in formerlyArr" :key="key">
+            <div class="left_content">
+              <p class="live_title">{{item.LIVETITLE}}</p>
+              <p class="live_teacher">{{item.LIVETEACHER}}</p>
+              <p class="live_time">{{item.LIVETIME}}</p>
             </div>
-            <div class="right-content">
-              <img src="/img/live/zyp.png" alt />
-            </div>
-          </div>
-          <div class="content-box">
-            <div class="left-content">
-              <img src="/img/live/ftlesson.png" alt />
-              <img src="/img/live/teacher-ft.png" alt />
-              <img src="/img/live/replayer728.png" alt />
-            </div>
-            <div class="right-content">
-              <img src="/img/live/ft.png" alt />
-            </div>
-          </div>
-          <div class="content-box">
-            <div class="left-content">
-              <img src="/img/live/syylesson.png" alt />
-              <img src="/img/live/teacher-syy.png" alt />
-              <img src="/img/live/replayer728.png" alt />
-            </div>
-            <div class="right-content">
-              <img src="/img/live/syy.png" alt />
-            </div>
-          </div>
-          <div class="content-box">
-            <div class="left-content">
-              <img src="/img/live/zjwlesson.png" alt />
-              <img src="/img/live/teacher-zjw.png" alt />
-              <img src="/img/live/replayer728.png" alt />
-            </div>
-            <div class="right-content">
-              <img src="/img/live/zjw.png" alt />
-            </div>
-          </div>
-          <div class="content-box">
-            <div class="left-content">
-              <img src="/img/live/hxwlesson.png" alt />
-              <img src="/img/live/teacher-hxw.png" alt />
-              <img src="/img/live/replayer728.png" alt />
-            </div>
-            <div class="right-content">
-              <img src="/img/live/hxw.png" alt />
+            <div class="right_content">
+              <img :src="item.PHOTOPATH" alt />
             </div>
           </div>
         </div>
@@ -99,8 +49,13 @@ import commonBanner from "./../common/banner.vue";
 export default {
   data () {
     return {
-
+      nowArr: [],
+      formerlyArr: []
     }
+  },
+  mounted () {
+    this.nowArr = this.i18n.interaction.LIVE.LIVENOW;
+    this.formerlyArr = this.i18n.interaction.LIVE.LIVEFORMERLY;
   },
   components: {
     commonBanner
@@ -110,14 +65,128 @@ export default {
 </script>
 
 <style lang='less'>
-.liveList {
-  background: url("/img/live/ball-background.png") no-repeat;
-  background-size: contain;
-  background-position: center center;
+@urlhead: "/img/live/";
+.background_css(@url,@repeat,@position,@size) {
+  background-image: url(@url);
+  background-repeat: @repeat;
+  background-position: @position;
+  background-size: @size;
+}
+@mr_left: left;
+@mr_right: right;
+@mr_bottom: bottom;
+@mr_top: top;
+.mr(@position,@px) when (@position = @mr_top) {
+  margin-top: @px;
+}
+.mr(@position,@px) when (@position = @mr_right) {
+  margin-right: @px;
+}
+.mr(@position,@px) when (@position = @mr_bottom) {
+  margin-bottom: @px;
+}
+.mr(@position,@px) when (@position = @mr_left) {
+  margin-left: @px;
+}
+.mr_All(@mr) {
+  margin: @mr;
+}
+.word_common_css(@fontSize,@fontFamily1,@fontFamily2,@fontWeight,@color,@lineHeight) {
+  font-size: @fontSize;
+  font-family: @fontFamily1, @fontFamily2;
+  font-weight: @fontWeight;
+  color: @color;
+  line-height: @lineHeight;
+}
+.title_css {
+  .word_common_css(24px,FZLTHJW--GB1-0, FZLTHJW--GB1,normal,rgba(0, 0, 0, 1),34px);
+  @media (max-width: 1000px) {
+    .word_common_css(16px,FZLTHJW--GB1-0, FZLTHJW--GB1,550,rgba(11, 22, 43, 1),26px);
+    margin-bottom: 10px;
+  }
+}
+.the_content_css {
+  display: flex;
+  .mr(left,5px);
+  @media (max-width: 1000px) {
+    .mr(left,0);
+  }
+}
+.content_box_css {
+  cursor: pointer;
+  .wid_and_hei(350px,200px);
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0px 6px 30px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  .mr_All(30px 0 0 25px);
+  &:first-of-type {
+    .mr(left,0);
+  }
+  @media (min-width: 1000px) and (max-width: 1680px) {
+    &:nth-of-type(4n + 0) {
+      .mr(left,0);
+    }
+  }
+  @media (min-width: 1680px) {
+    &:nth-of-type(5n + 0) {
+      .mr(left,0);
+    }
+  }
+  @media (max-width: 1000px) {
+    .wid_and_hei(315px,166px);
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 3px 10px 0px rgba(0, 0, 0, 0.1);
+    margin: 30px 0 0 0;
+  }
+}
+.wid_and_hei(@width,@height) {
+  width: @width;
+  height: @height;
+}
+.right_content_css {
+  float: left;
+  .wid_and_hei(145px,200px);
+  .background_css("@{urlhead}Fill.svg",no-repeat,0 0,contain);
+  img {
+    .wid_and_hei(109px,109px);
+    .mr_All(62px 12px 29px 24px);
+  }
+  @media (max-width: 1000px) {
+    float: left;
+    .wid_and_hei(130px,166px);
+    .background_css("@{urlhead}Fill.svg",no-repeat,3px -8px,contain);
+    img {
+      .wid_and_hei(90px,90px);
+      .mr_All(50px 18px 39px 22px);
+    }
+  }
+}
+.left_content_css {
+  float: left;
+  width: 205px;
+  @media (max-width: 1000px) {
+    float: left;
+    width: 185px;
+  }
+}
+.mobile_word_css(@fontSize,@fontFamily1,@fontFamily2,@fontWeight,@color,@lineHeight) {
+  @media (max-width: 1000px) {
+    max-width: 129px;
+    max-height: 52px;
+    font-size: @fontSize;
+    font-family: @fontFamily1, @fontFamily2;
+    font-weight: @fontWeight;
+    color: @color;
+    line-height: @lineHeight;
+  }
+}
+// 具体样式从这里开始
+.live_ist {
+  .background_css("@{urlhead}ball-background.png",no-repeat,center center,contain);
   margin-top: 68px;
   @media (max-width: 1000px) {
     background: none;
-    margin: 40px 0 80px 0;
+    .mr_All(40px 0 80px 0);
   }
   .block {
     height: 200px;
@@ -126,243 +195,106 @@ export default {
     }
   }
 }
-.liveList .now-start {
-  margin-left: 200px;
+.live_ist .now_start {
+  .mr(left,200px);
   @media (max-width: 1000px) {
-    margin-left: 30px;
+    .mr(left,30px);
   }
-  .now-start-tile {
-    font-size: 24px;
-    font-family: FZLTHJW--GB1-0, FZLTHJW--GB1;
-    font-weight: normal;
-    color: rgba(0, 0, 0, 1);
-    line-height: 34px;
-    margin-bottom: 30px;
-    @media (max-width: 1000px) {
-      font-size: 16px;
-      font-family: FZLTHJW--GB1-0, FZLTHJW--GB1;
-      font-weight: normal;
-      color: rgba(11, 22, 43, 1);
-      line-height: 26px;
-      font-weight: 550;
-      margin-bottom: 10px;
-    }
+  .now_start_title {
+    .title_css;
   }
-  .now-start-content {
-    display: flex;
+  .now_start_content {
+    .the_content_css;
     flex-direction: row;
-    margin-left: 5px;
     @media (max-width: 1000px) {
       flex-direction: column;
-      margin-left: 0;
     }
-    .content-box {
-      cursor: pointer;
-      width: 350px;
-      height: 200px;
-      background: rgba(255, 255, 255, 1);
-      box-shadow: 0px 6px 30px 0px rgba(0, 0, 0, 0.1);
-      border-radius: 8px;
-      margin-left: 25px;
-      &:first-of-type {
-        margin-left: 0;
-      }
-      @media (max-width: 1000px) {
-        width: 315px;
-        height: 166px;
-        background: rgba(255, 255, 255, 1);
-        box-shadow: 0px 3px 10px 0px rgba(0, 0, 0, 0.1);
-        margin: 30px 0 0 0;
-      }
-      .left-content {
-        float: left;
-        width: 205px;
-        img {
-          margin: 24px 0 0 20px;
-          &:first-of-type {
-            width: 140px;
-            height: 30px;
-          }
-          &:nth-of-type(2) {
-            width: 84px;
-            height: 14px;
-          }
+    .content_box {
+      .content_box_css;
+      .left_content {
+        .left_content_css;
+        p {
+          .mr_All(24px 0 0 20px);
           &:last-of-type {
-            width: 142px;
-            height: 14px;
-            margin-top: 12px;
+            .mr(top,12px);
           }
         }
-        @media (max-width: 1000px) {
-          float: left;
-          width: 185px;
-          img {
-            margin: 24px 0 0 20px;
-            &:first-of-type {
-              width: 101px;
-              height: 26px;
-            }
-            &:nth-of-type(2) {
-              width: 84px;
-              height: 24px;
-            }
-            &:last-of-type {
-              width: 142px;
-              height: 24px;
-              margin-top: 10px;
-            }
+        .live_title {
+          max-width: 180px;
+          max-height: 60px;
+          .word_common_css(20px,FZLTHJW--GB1-0,FZLTHJW--GB1,normal,rgba(0, 0, 0, 1),30px);
+          @media (max-width: 1000px) {
+            .word_common_css(14px,FZLTHJW--GB1-0,FZLTHJW--GB1,normal,rgba(0,0,0,1),26px);
+            max-width: 129px;
+            max-height: 52px;
+          }
+        }
+        .live_teacher {
+          .word_common_css(14px,PingFangSC-Regular,PingFang SC,400,rgba(0, 0, 0, 1),14px);
+          @media (max-width: 1000px) {
+            .word_common_css(14px,FZLTHJW--GB1-0,FZLTHJW--GB1,normal,rgba(0,0,0,0.5),24px);
+          }
+        }
+        .live_time {
+          .word_common_css(14px,PingFangSC-Regular,PingFang SC,400,rgba(0, 0, 0, 1),14px);
+          @media (max-width: 1000px) {
+            .word_common_css(14px,FZLTHJW--GB1-0,FZLTHJW--GB1,normal,rgba(0,0,0,0.5),24px);
           }
         }
       }
-      .right-content {
-        float: left;
-        width: 145px;
-        height: 200px;
-        background: url("/img/live/Fill2.svg") no-repeat;
-        background-size: contain;
-        img {
-          width: 109px;
-          height: 109px;
-          margin: 62px 12px 29px 24px;
-        }
-        @media (max-width: 1000px) {
-          float: left;
-          width: 130px;
-          height: 166px;
-          background: url("/img/live/Fill2.svg") no-repeat;
-          background-size: contain;
-          background-position: 3px -8px;
-          img {
-            width: 90px;
-            height: 90px;
-            margin: 50px 18px 39px 22px;
-          }
-        }
+      .right_content {
+        .right_content_css;
       }
     }
   }
 }
 
-.liveList .replayer {
-  margin: 50px 0 0 200px;
+.live_ist .replayer {
+  .mr_All(50px 0 0 200px);
   @media (max-width: 1000px) {
-    margin: 40px 0 0 30px;
+    .mr_All(40px 0 0 30px);
   }
-  .replayer-tile {
-    font-size: 24px;
-    font-family: FZLTHJW--GB1-0, FZLTHJW--GB1;
-    font-weight: normal;
-    color: rgba(0, 0, 0, 1);
-    line-height: 34px;
-    margin-bottom: 30px;
-    @media (max-width: 1000px) {
-      font-size: 16px;
-      font-family: FZLTHJW--GB1-0, FZLTHJW--GB1;
-      font-weight: normal;
-      color: rgba(11, 22, 43, 1);
-      line-height: 26px;
-      font-weight: 550;
-      margin-bottom: 10px;
-    }
+  .replayer_tile {
+    .title_css;
   }
-  .replayer-content {
-    display: flex;
-    margin-left: 5px;
+  .replayer_content {
+    .the_content_css;
     flex-wrap: wrap;
-    @media (max-width: 1000px) {
-      margin-left: 0;
-    }
-    .content-box {
-      cursor: pointer;
-      width: 350px;
-      height: 200px;
-      background: rgba(255, 255, 255, 1);
-      box-shadow: 0px 6px 30px 0px rgba(0, 0, 0, 0.1);
-      border-radius: 8px;
-      margin-left: 25px;
-      &:first-of-type {
-        margin-left: 0;
-      }
-      &:nth-of-type(4) {
-        margin: 30px 0 0 0;
-      }
-      &:last-of-type {
-        margin-top: 30px;
-      }
-      @media (max-width: 1000px) {
-        width: 315px;
-        height: 166px;
-        background: rgba(255, 255, 255, 1);
-        box-shadow: 0px 3px 10px 0px rgba(0, 0, 0, 0.1);
-        margin: 30px 0 0 0;
-        content: "";
-        clear: both;
-        display: block;
-      }
-      .left-content {
-        float: left;
-        width: 205px;
-        img {
-          margin: 24px 0 0 20px;
-          &:first-of-type {
-            width: 140px;
-            height: 30px;
-          }
-          &:nth-of-type(2) {
-            width: 84px;
-            height: 14px;
-          }
+    .content_box {
+      .content_box_css;
+      .left_content {
+        .left_content_css;
+        p {
+          .mr_All(24px 0 0 20px);
           &:last-of-type {
-            width: 142px;
-            height: 14px;
-            margin-top: 12px;
+            .mr(top,12px);
           }
         }
-        @media (max-width: 1000px) {
-          float: left;
-          width: 185px;
-          img {
-            margin: 24px 0 0 20px;
-            &:first-of-type {
-              width: 101px;
-              height: 26px;
-            }
-            &:nth-of-type(2) {
-              width: 84px;
-              height: 24px;
-            }
-            &:last-of-type {
-              width: 142px;
-              height: 24px;
-              margin-top: 10px;
-            }
+        .live_title {
+          max-width: 180px;
+          max-height: 60px;
+          .word_common_css(20px,FZLTHJW--GB1-0,FZLTHJW--GB1,normal,rgba(0, 0, 0, 1),30px);
+          @media (max-width: 1000px) {
+            .word_common_css(14px,FZLTHJW--GB1-0,FZLTHJW--GB1,normal,rgba(0,0,0,1),26px);
+            max-width: 129px;
+            max-height: 52px;
+          }
+        }
+        .live_teacher {
+          .word_common_css(14px,PingFangSC-Regular,PingFang SC,400,rgba(0, 0, 0, 1),14px);
+          @media (max-width: 1000px) {
+            .word_common_css(14px,FZLTHJW--GB1-0,FZLTHJW--GB1,normal,rgba(0,0,0,0.5),24px);
+          }
+        }
+        .live_time {
+          .word_common_css(14px,PingFangSC-Regular,PingFang SC,400,rgba(0, 0, 0, 1),14px);
+          @media (max-width: 1000px) {
+            .word_common_css(14px,FZLTHJW--GB1-0,FZLTHJW--GB1,normal,rgba(0,0,0,0.5),24px);
           }
         }
       }
-      .right-content {
-        float: left;
-        width: 145px;
-        height: 200px;
-        background: url("/img/live/Fill.svg") no-repeat;
-        background-size: contain;
-        img {
-          width: 109px;
-          height: 109px;
-          margin: 62px 12px 29px 24px;
-        }
-        @media (max-width: 1000px) {
-          float: left;
-          width: 130px;
-          height: 166px;
-          background: url("/img/live/Fill.svg") no-repeat;
-          background-size: contain;
-          background-position: 3px -8px;
-          img {
-            width: 90px;
-            height: 90px;
-            margin: 50px 18px 39px 22px;
-          }
-        }
+      .right_content {
+        .right_content_css;
       }
     }
   }
--- web-ui/docs/.vuepress/lang/lang-modules/interaction.js
+++ web-ui/docs/.vuepress/lang/lang-modules/interaction.js
@@ -6,14 +6,102 @@ module.exports = {
         LIVE: {
             LIVE: '直播',
             WILLPLAYER: '即将开始',
-            REPLAYER: '精彩回顾'
+            REPLAYER: '精彩回顾',
+            LIVENOW: [{
+                    LIVETITLE: '虚拟化技术介绍',
+                    LIVETEACHER: '讲师: 张海亮',
+                    LIVETIME: '时间: 7月30日 20:00',
+                    PHOTOPATH: '/img/live/zhl.png'
+                },
+                {
+                    LIVETITLE: '安装虚拟化&管理虚拟机',
+                    LIVETEACHER: '讲师: 陈振东',
+                    LIVETIME: '时间: 7月30日 20:00',
+                    PHOTOPATH: '/img/live/czd.png'
+                }
+            ],
+            LIVEFORMERLY: [{
+                    LIVETITLE: '初始openEuler',
+                    LIVETEACHER: '讲师: 朱延朋',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/zyp.png'
+                },
+                {
+                    LIVETITLE: '安装openEuler',
+                    LIVETEACHER: '讲师: 冯涛',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/ft.png'
+                },
+                {
+                    LIVETITLE: '使用openEuler',
+                    LIVETEACHER: '讲师: 沈洋洋',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/syy.png'
+                },
+                {
+                    LIVETITLE: 'openEuler构建之OBS使用指导',
+                    LIVETEACHER: '讲师: 朱健伟',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/zjw.png'
+                },
+                {
+                    LIVETITLE: 'openEuler软件包的构建、开发与维护',
+                    LIVETEACHER: '讲师: 何晓文',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/hxw.png'
+                },
+            ]
         }
     },
     en: {
         LIVE: {
             LIVE: 'Live',
             WILLPLAYER: 'About to begin',
-            REPLAYER: 'Wonderful review'
+            REPLAYER: 'Wonderful review',
+            LIVENOW: [{
+                    LIVETITLE: '虚拟化技术介绍',
+                    LIVETEACHER: '讲师: 张海亮',
+                    LIVETIME: '时间: 7月30日 20:00',
+                    PHOTOPATH: '/img/live/zhl.png'
+                },
+                {
+                    LIVETITLE: '安装虚拟化&管理虚拟机',
+                    LIVETEACHER: '讲师: 陈振东',
+                    LIVETIME: '时间: 7月30日 20:00',
+                    PHOTOPATH: '/img/live/czd.png'
+                }
+            ],
+            LIVEFORMERLY: [{
+                    LIVETITLE: '初始openEuler',
+                    LIVETEACHER: '讲师: 朱延朋',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/zyp.png'
+                },
+                {
+                    LIVETITLE: '安装openEuler',
+                    LIVETEACHER: '讲师: 冯涛',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/ft.png'
+                },
+                {
+                    LIVETITLE: '使用openEuler',
+                    LIVETEACHER: '讲师: 沈洋洋',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/syy.png'
+                },
+                {
+                    LIVETITLE: 'openEuler构建之OBS使用指导',
+                    LIVETEACHER: '讲师: 朱健伟',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/zjw.png'
+                },
+                {
+                    LIVETITLE: 'openEuler软件包的构建、开发与维护',
+                    LIVETEACHER: '讲师: 何晓文',
+                    LIVETIME: '时间: 7月28日 20:00',
+                    PHOTOPATH: '/img/live/hxw.png'
+                },
+            ]
         }
     }
 };
