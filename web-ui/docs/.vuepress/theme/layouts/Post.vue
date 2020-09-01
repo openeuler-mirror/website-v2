552949d78434f7e8e5528d6043c5bdd3dd449ebe
wubenxue <1364610043@qq.com>
2020-09-01 10:51:13 +0800

    修改行内样式

--- web-ui/docs/.vuepress/theme/layouts/Post.vue
+++ web-ui/docs/.vuepress/theme/layouts/Post.vue
@@ -59,8 +59,8 @@
         </p>
       </div>
       <p class="blog-item-tag">
-        <span style="color:#00000080">{{i18n.community.BLOG.LABEL}}:</span>
-        <span v-for="(tag, index) in $frontmatter.tags" :key="index" style="color:#002fa7">
+        <span>{{i18n.community.BLOG.LABEL}}:</span>
+        <span v-for="(tag, index) in $frontmatter.tags" :key="index">
           <span class="tag-item">{{tag}}</span>
           <span v-if="index != ($frontmatter.tags.length - 1)">、</span>
         </span>
@@ -310,6 +310,9 @@ export default {
   }
   .blog-item-tag {
       display: none;
+      .first-tag{
+        color:#00000080
+      }
     }
   .disclaimer {
     font-size: 16px;
