24b261de9eda7ff7661d009f7732cbc99e96ed88
zhangxiaopan <zhangxiaopan2@huawei.com>
2020-09-03 16:58:13 +0800

    增加包管理搜索接口

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,15 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const os = require('os');
 const url = require('url');
 const path = require('path');
 const CONF = require('../config/filePathConfig');
+const HTTP = require('../util/httpUtil');
+const ES = require('../config/searchConfig');
+const logUtil = require('../util/logUtil');
+const REPO_ES_INDEX = 'openeuler';
+const OPENEULER_URL = 'https://repo.openeuler.org';
 
 router.get('/', function (req, res, next) {
     res.json({
@@ -83,6 +89,46 @@ function getLineArr(line) {
         child: []
     }
     return arr;
+}
+
+router.get('/repo/search', function (req, res, next) {
+    let obj = url.parse(encodeURI(req.url), true);
+    let json = {
+        query: {
+            wildcard: {
+                'file.filename': {
+                    'value': '*' + obj.query.keyword + '*'
+                }
+            }
+        }
+    };
+    let token = new Buffer.from(ES.ES_USER_PASS).toString('base64');
+    let now = logUtil.getTime();
+    HTTP.postES(ES.ES_URL + REPO_ES_INDEX + '/_doc/_search', token, json).then(data => {
+        let responseData = getSearchResJson(data);
+        res.send(responseData);
+    }).catch(ex => {
+        console.log('[' + now + ']' + ex.stack + os.EOL);
+        logUtil.errorLogfile.write('[' + now + ']' + ex.stack + os.EOL);
+    });
+});
+
+function getSearchResJson(data) {
+    let arr = [];
+    let num = data.hits.total.value;
+    data.hits.hits.forEach(element => {
+        element._source.path.virtual = OPENEULER_URL + element._source.path.virtual;
+        arr.push(element._source);
+    });
+    let json = {
+        'status': 200,
+        'msg': '',
+        'data': {
+            'totalNum': num,
+            'records': arr
+        }
+    };
+    return json;
 }
 
 module.exports = router;
