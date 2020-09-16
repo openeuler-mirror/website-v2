ac2617e821b029870a7e177517b4f5f6068a50f5
Zoujinda <826835384@qq.com>
2020-09-16 11:03:05 +0800

    美化滚动条

--- web-ui/docs/.vuepress/theme/layouts/DocDetails.vue
+++ web-ui/docs/.vuepress/theme/layouts/DocDetails.vue
@@ -286,8 +286,25 @@ export default {
   margin-bottom: 400px;
 }
 .el-tree{
-    max-height: 540px;
-    overflow-y: scroll;
+    height: 400px;
+    overflow-y: hidden;
+    &::-webkit-scrollbar-track {
+        -webkit-box-shadow:inset 0 0 6px rgba(0,0,0,0.3);
+        border-radius:10px;
+        background-color:#F5F5F5;
+    }
+    &::-webkit-scrollbar {
+        width:6px;
+        background-color:#F5F5F5;
+    }
+    &::-webkit-scrollbar-thumb {
+        border-radius:10px;
+        -webkit-box-shadow:inset 0 0 6px rgba(0,0,0,.3);
+        background-color:rgba(144,147,153,.3);
+    }
+    &:hover{
+        overflow-y: scroll;
+    }
 }
 .mask {
   position: fixed;
