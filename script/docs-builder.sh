a1e396af78c82806b2d7aef2ffdcfca70cc6d0f8
zhangxiaopan <zhangxiaopan2@huawei.com>
2020-09-23 17:11:23 +0800

    脚本调整

--- script/docs-builder.sh
+++ script/docs-builder.sh
@@ -9,6 +9,13 @@ git clone https://gitee.com/openeuler/docs
 
 cd ./docs
 
+mkdir -p ../server/web-ui/docs/zh
+mkdir -p ../server/web-ui/docs/en
+cp -r ../web-ui/docs/zh/blog ../server/web-ui/docs/zh
+cp -r ../web-ui/docs/en/blog ../server/web-ui/docs/en
+cp -r ../web-ui/docs/zh/news ../server/web-ui/docs/zh
+cp -r ../web-ui/docs/en/news ../server/web-ui/docs/en
+
 for r in $(git branch -r --list "origin/stable2-*")
 do
   b=${r##*origin/stable2-}
