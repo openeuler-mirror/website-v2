016400140776bb3f8ddf16a7c5931917c0484490
Zoujindaa <8268353842@qq.com>
2020-11-21 18:29:43 +0800

    迭代倒计时组件

--- web-ui/docs/.vuepress/components/timer/countdown.vue
+++ web-ui/docs/.vuepress/components/timer/countdown.vue
@@ -1,46 +1,41 @@
 <template>
-    <div class="count-down">
-        <p class="timeText">距离活动开始还有：</p>
-        <p class="timeValue">
-            <span class="day">{{ showTime.day }}</span>
-            <span>:</span>
-            <span class="hour">{{ showTime.hour }}</span>
-            <span>:</span>
-            <span class="minute">{{ showTime.minute }}</span>
-            <span>:</span>
-            <span class="second">{{ showTime.second }}</span>
-        </p>
-        <p class="timeDesc">
-            <span>DAYS</span>
-            <span>HOURS</span>
-            <span>MIN</span>
-            <span>SEC</span>
-        </p>
+    <div class="count-down" :style="{backgroundImage: 'url(' + timeImage + ')'}">
+        <div class="started">
+            <p class="timeText">{{ i18n.timer.TIMETEXT.STARTED }}</p>
+            <p class="timeValue">
+                <span class="day">{{ showTime.day }}</span>
+                <span>:</span>
+                <span class="hour">{{ showTime.hour }}</span>
+                <span>:</span>
+                <span class="minute">{{ showTime.minute }}</span>
+                <span>:</span>
+                <span class="second">{{ showTime.second }}</span>
+            </p>
+            <p class="timeDesc">
+                <span v-for="(item,index) in i18n.timer.TIMEDESC">{{ item }}</span>
+            </p>
+        </div>
     </div>
 </template>
 
 <script>
+
 export default {
     data() {
         return {
-            targetTime: {
-                year: 2020,
-                month: 11,
-                day: 24,
-                hour: 0,
-                minute: 0,
-                second: 0,
-                time: 0
-            },
+            timeText: '',
             showTime: {
                 day: '',
                 hour: '',
                 minute: '',
                 second: ''
             },
-            timer: null
+            timer: null,
+            targetValue:0
         }
     },
+    //开始时间和结束时间形式：2020-(年|/|.)12-(月|/|.)23(日) 12:()00
+    props: ["targetTime","startTime","timeImage"],
     created() {
         this.getTargetTime();
     },
@@ -55,19 +50,33 @@ export default {
     },
     methods:{
         getTargetTime() {
-            //new Date(year,month,day,hour,min,sec) 得到某日期的日期对象
-            let year = this.targetTime.year;
-            let month = this.targetTime.month;
-            let day = this.targetTime.day;
-            let hour = this.targetTime.hour;
-            let min = this.targetTime.minute;
-            let sec = this.targetTime.second;
-            let time2 = new Date(year,month,day,hour,min,sec);
-            this.targetTime.time = parseInt(time2.getTime());
+            //new Date(year,month,day,hour,min,sec) 得到某日期的日期对象 month:0~11
+            let timeArr = this.targetTime.split(" ");
+            let year = timeArr[0].slice(0,4);
+            let month = 0;
+            let day = 0;
+            let hour = 0;
+            let min = 0;
+            if(isNaN(timeArr[0].slice(6,7))) {
+                month = timeArr[0].slice(5,6) - 1;
+                day = timeArr[0].slice(7,9);
+            }else {
+                month = timeArr[0].slice(5,7) - 1;
+                day = timeArr[0].slice(8,10);
+            }
+            if(isNaN(timeArr[1].slice(1,2))) {
+                hour = timeArr[1].slice(0,1);
+                min = timeArr[1].slice(2,4);
+            }else {
+                hour = timeArr[1].slice(0,2);
+                min = timeArr[1].slice(3,5);
+            }
+            let time2 = new Date(year,month,day,hour,min,0);
+            this.targetValue = parseInt(time2.getTime());
         },
         calculateTime() {
             let currentTime = new Date().getTime();
-            let result = (this.targetTime.time - currentTime) / 1000;
+            let result = (this.targetValue - currentTime) / 1000;
             let resDay = Math.floor(result / (3600 * 24));
             result = result % (3600 * 24);
             let resHour = Math.floor(result / 3600);
@@ -92,34 +101,33 @@ export default {
     width: 400px;
     height: 150px;
     margin: 0 auto;
-    background-image: url('/img/summit/time-background.jpg');
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center center;
     color: white;
-    p{
+    .started {
+        p{
         margin-bottom: 20px;
-        text-align: center;
-    }
-    .timeText{
-        font-size: 18px;
-    }
-    .timeValue{
-        span{
-            font-size: 30px;
-            display: inline-block;
-            margin-right: 9px;
+            text-align: center;
         }
-    }
-    .timeDesc{
-        span{
-            font-size: 16px;
-            display: inline-block;
-            margin-right: 15px;
-            &:nth-of-type(3) {
-                margin-right: 28px;
+        .timeText{
+            font-size: 18px;
+        }
+        .timeValue{
+            span{
+                font-size: 30px;
+                display: inline-block;
+                margin-right: 9px;
+            }
+        }
+        .timeDesc{
+            span{
+                font-size: 16px;
+                display: inline-block;
+                margin-right: 15px;
             }
         }
     }
+    
 }
 </style>
--- web-ui/docs/.vuepress/lang/zh.js
+++ web-ui/docs/.vuepress/lang/zh.js
@@ -11,8 +11,8 @@ module.exports = {
     sig: require('./lang-modules/sig').cn,
     authentication: require('./lang-modules/authentication').cn,
     security: require('./lang-modules/security').cn,
-    security: require('./lang-modules/security').cn,
     interaction: require('./lang-modules/interaction').cn,
     brand: require('./lang-modules/brand').cn,
     search: require('./lang-modules/search').cn,
+    timer: require('./lang-modules/timer').cn
 };
