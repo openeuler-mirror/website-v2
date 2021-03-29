cdf7ebb5b32396535c0346a2a75f217b457d61ea
xzx <563905150@qq.com>
2021-03-29 20:12:03 +0800

    modify link position

--- web-ui/docs/.vuepress/components/summit/summit-devday.vue
+++ web-ui/docs/.vuepress/components/summit/summit-devday.vue
@@ -9,18 +9,19 @@
         </div>
         <div class="text-wrapper">
             <p class="text">openEuler Developer Day 2021 是由 openEuler 社区发起，面向社区开发者的一场技术盛会。旨在推动 openEuler 社区在多样性计算、云原生全栈、全场景协同等技术发展方向的持续探索和创新。本次大会以开放工作会议以及小组讨论等形式，让开发者找到关注的技术小组、快速融入社区、围绕下一版本发布，热点技术方向，用户核心需求，展开面对面的头脑风暴。每一个开发者都是 openEuler 社区的一颗星，一滴水，最终点点滴滴汇聚成科技创新的星辰大海。</p>
+            <div class="link-wrapper">
+                <a href="https://shimo.im/forms/KgXxt8JCYKYtVXHx/fill" target="_blank"><img src="/img/summit/devday-2021/link-img1.png"></a>
+                <a href="https://shimo.im/forms/vwCC68q3pQvvR3Yc/fill" target="_blank"><img src="/img/summit/devday-2021/link-img2.png"></a>
+                <a href="https://shimo.im/forms/x6vXyW8QHCGK9QJD/fill" target="_blank"><img src="/img/summit/devday-2021/link-img3.png"></a>
+                <a href="https://shimo.im/forms/PQTvWKjHgg96DjD8/fill" target="_blank"><img src="/img/summit/devday-2021/link-img4.png"></a>
+            </div>
             <p class="title">openEuler 开放工作会议介绍：</p>
             <p class="text">openEuler 技术委员会开放工作会议：会议将集中探讨 openEuler 技术路线和下一版本的发布计划</p>
             <p class="text">openEuler 用户委员会开放工作会议：会议将集中收集用户在技术落地应用中碰到的问题，以及系统迁移路径的实践和分享</p>
             <p class="text">openEuler 品牌委员会开放工作会议：会议将对 openEuler 社区的营销传播推广工作进行梳理和介绍</p>
             <p class="text">openEuler SIG 小组开放工作会：在社区公开招募 20-30 个 SIG，小组成员在 SIG Maintainer 的带领下，召开现场工作会议，落实兴趣小组的工作重点、接受社区公开需求、完成下一个版本的开发路线规划。</p>
         </div>
-        <div class="link-wrapper">
-            <a href="https://shimo.im/forms/KgXxt8JCYKYtVXHx/fill" target="_blank"><img src="/img/summit/devday-2021/link-img1.png"></a>
-            <a href="https://shimo.im/forms/vwCC68q3pQvvR3Yc/fill" target="_blank"><img src="/img/summit/devday-2021/link-img2.png"></a>
-            <a href="https://shimo.im/forms/x6vXyW8QHCGK9QJD/fill" target="_blank"><img src="/img/summit/devday-2021/link-img3.png"></a>
-            <a href="https://shimo.im/forms/PQTvWKjHgg96DjD8/fill" target="_blank"><img src="/img/summit/devday-2021/link-img4.png"></a>
-        </div>
+        
         <div class="review-wrapper">
             <div class="title">
                 <span>精彩回顾</span>
@@ -98,18 +99,16 @@ export default {
         color: #000;
         font-size: 20px;
         line-height: 40px;
-        .title {
-            margin-top: 20px;
-        }
         @media screen and (max-width: 1000px) {
             font-size: 14px;
             line-height: 26px;
         }
     }
     .link-wrapper {
-        margin-bottom: 60px;
+        margin: 20px 0;
         display: flex;
         justify-content: space-between;
+        line-height: 1;
         @media screen and (max-width: 1000px) {
             flex-direction: column;
             justify-content: center;
