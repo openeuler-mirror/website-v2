e68cd1d83d022fef47f7c36410199efebf4eb9d0
zoujinda <826835384@qq.com>
2021-04-14 14:13:17 +0800

    Repair the brand page
    save
    
    save
    
    save brand
    
    save

--- web-ui/docs/.vuepress/lang/lang-modules/brand.js
+++ web-ui/docs/.vuepress/lang/lang-modules/brand.js
@@ -5,209 +5,257 @@
 module.exports = {
     cn: {
         BRAND: '品牌',
-        NOTICE:'注意',
         WORDS:'openEuler网站上使用和显示的所有商标、标志皆属openEuler社区所有，但注明属于其他方拥有的商标、标志除外。未经openEuler社区或其他方书面许可，openEuler网站所载的任何内容不应被视作以暗示、不反对或其他形式授予使用前述任何商标、标志的许可或权利。未经事先书面许可，任何人不得以任何方式使用openEuler社区的名称及openEuler社区的商标、标记，除非你使用前述名称、商标和标记是出于个人、教育和非商业目的。任何人不得在未获得第三方同意的情况下使用第三方拥有的商标和标志。',
         MOBILETITLE:'商标',
-        VERTICAL:'Vertical',
-        HORIZONTAL:'Horizontal',
+        PPT_TEXT: 'PPT模板',
         PICTURE_TITLE:['Standard','Monochrome','Black','Standard','White','Black'],
         VERTICAL_LEFT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-left.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-left.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-left.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-left.pdf'
         }],
         VERTICAL_CENTER_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-center.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-center.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-center.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-center.pdf'
         }],
         VERTICAL_RIGHT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-right.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-right.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-right.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-right.pdf'
         }],
         HORIZONTAL_LEFT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-left.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-left.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-left.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-left.pdf'
         }],
         HORIZONTAL_CENTER_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-center.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-center.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-center.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-center.pdf'
         }],
         HORIZONTAL_RIGHT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-right.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-right.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-right.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-right.pdf'
-        }]
+        }],
+        PPT_LIST: [
+            {
+                URL: '/img/other/brand/zh-ppt1.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/ppt01.pptx'
+            },
+            {
+                URL: '/img/other/brand/zh-ppt2.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/ppt02.pptx'
+            },
+            {
+                URL: '/img/other/brand/zh-ppt3.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/ppt03.pptx'
+            },
+            {
+                URL: '/img/other/brand/zh-ppt4.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/ppt04.pptx'
+            }
+        ]
     },
     en: {
         BRAND: 'Brand',
-        NOTICE:'Notice',
         WORDS:'Notice: All logos and trademarks used on this website are the properties of openEuler community or other third parties as stated if applicable. No content provided on the openEuler community website shall be deemed as granting approval or the right to use any trademark or logo aforesaid by implication, lack of objection, or other means without the prior written consent of openEuler community or any third party which may own the mark. No individual shall use the name, trademark, and logo of openEuler community by any means without the prior written consent of openEuler community, except that your use of the name, trademark, and logo aforesaid is solely for personal, educational and non-commercial purpose. No individual shall use trademarks and logos owned by third parties without getting their approval.',
-        MOBILETITLE:'Brand',
-        VERTICAL:'Vertical',
-        HORIZONTAL:'Horizontal',
+        MOBILETITLE:'Trademark',
+        PPT_TEXT: 'PPT Templates',
         PICTURE_TITLE:['Standard','Monochrome','Black','Standard','White','Black'],
         VERTICAL_LEFT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-left.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-left.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-left.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-left.pdf'
         }],
         VERTICAL_CENTER_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-center.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-center.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-center.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-center.pdf'
         }],
         VERTICAL_RIGHT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-right.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-right.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-right.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-right.pdf'
         }],
         HORIZONTAL_LEFT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-left.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-left.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-left.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-left.pdf'
         }],
         HORIZONTAL_CENTER_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-center.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-center.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-center.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-center.pdf'
         }],
         HORIZONTAL_RIGHT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-right.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-right.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-right.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-right.pdf'
-        }]
+        }],
+        PPT_LIST: [
+            {
+                URL: '/img/other/brand/en-ppt1.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/enppt01.pptx'
+            },
+            {
+                URL: '/img/other/brand/en-ppt2.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/enppt02.pptx'
+            },
+            {
+                URL: '/img/other/brand/en-ppt3.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/enppt03.pptx'
+            },
+            {
+                URL: '/img/other/brand/en-ppt4.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/enppt04.pptx'
+            }
+        ]
     },
     ru: {
         BRAND: 'Бренд',
-        NOTICE:'Примечание',
         WORDS:'Примечание. Все используемые на этом веб-сайте логотипы и товарные знаки являются собственностью сообщества openEuler или других указанных третьих лиц, если это применимо. Размещенные на веб-сайте сообщества openEuler материалы не должны рассматриваться как предоставленное разрешение или право использовать любой такой товарный знак или логотип косвенным путем, в силу отсутствия возражений или по другой причине без предварительного письменного согласия сообщества openEuler или любого третьего лица, которое может владеть данным товарным знаком. Запрещается кому-бы то ни было использовать любым способом название, товарный знак и логотип сообщества openEuler без предварительного письменного согласия последнего в любых целях, кроме личных, образовательных и некоммерческих. Запрещается кому-бы то ни было использовать товарные знаки и логотипы, принадлежащие третьим лицам, без их согласия.',
-        MOBILETITLE:'Бренд',
-        VERTICAL:'По вертикали',
-        HORIZONTAL:'По горизонтали',
+        MOBILETITLE:'Trademark',
+        PPT_TEXT: 'PPT Templates',
         PICTURE_TITLE:['Стандартный','Черно-белый','Черный','Стандартный','Белый','Черный'],
         VERTICAL_LEFT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-left.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-left.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-left.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-left.pdf'
         }],
         VERTICAL_CENTER_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-center.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-center.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-center.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-center.pdf'
         }],
         VERTICAL_RIGHT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/vertical-right.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/vertical-right.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/vertical-right.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/vertical-right.pdf'
         }],
         HORIZONTAL_LEFT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-left.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-left.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-left.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-left.pdf'
         }],
         HORIZONTAL_CENTER_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-center.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-center.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-center.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-center.pdf'
         }],
         HORIZONTAL_RIGHT_IMAGE:[{
+            STYLE:'JPG',
+            URL:'/img/other/brand/horizontal-right.jpg'
+        },{
             STYLE:'PNG',
             URL:'/img/other/brand/horizontal-right.png'
         },{
             STYLE:'SVG',
             URL:'/img/other/brand/horizontal-right.svg'
-        },{
-            STYLE:'PDF',
-            URL:'/img/other/brand/horizontal-right.pdf'
-        }]
+        }],
+        PPT_LIST: [
+            {
+                URL: '/img/other/brand/en-ppt1.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/enppt01.pptx'
+            },
+            {
+                URL: '/img/other/brand/en-ppt2.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/enppt02.pptx'
+            },
+            {
+                URL: '/img/other/brand/en-ppt3.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/enppt03.pptx'
+            },
+            {
+                URL: '/img/other/brand/en-ppt4.png',
+                LINK: 'https://openeuler-website.obs.ap-southeast-1.myhuaweicloud.com/PPT/enppt04.pptx'
+            }
+        ]
     }
 };
